b7a94c7db16b5116031c36d63478d1c0e8aac424
dystewart <dystewar@redhat.com>
2023-01-13 14:54:53 -0500

    Refine fio command to include regular output and run tests for 60 seconds

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -2,8 +2,8 @@
 set -ue
 cd /disk/target
 echo "Running jobs in fio-jobs.yaml in " $(pwd)":"
-fio --output disk-output.json --output-format json /fio-jobs/fio-jobs.fio
+fio --output disk-output.json --output-format=json,normal --runtime=60 /fio-jobs/fio-jobs.fio
 cd /pvc/target
 echo "Running jobs in fio-jobs.yaml in " $(pwd)":"
-fio --output pv-output.json --output-format json /fio-jobs/fio-jobs.fio
+fio --output pv-output.json --output-format=json,normal --runtime=60 /fio-jobs/fio-jobs.fio
 echo "Tests finished."
